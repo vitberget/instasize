eae2a8331712ba864ce4e362263e770697e95925
Kenneth Hedman <github@vitberget.se>
2025-01-30 21:26:48 +0100

    nicer clap

--- instasize/src/args.rs
+++ instasize/src/args.rs
@@ -1,37 +1,39 @@
 use anyhow::bail;
-use clap::{command, Parser};
+use clap::{command, Parser, ValueHint};
 use image::{Pixel, Rgba};
 use regex::Regex;
 
+/// Takes the source image and makes an image suitable for Instagram, aka 4/5 aspect ratio.
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
-/// Takes the source image and makes an image suitable for Instagram, aka 4/5 aspect ratio.
 pub struct InstasizeArgs {
-    /// Source image
+    /// Source image or directory with source images
+    #[arg(value_hint = ValueHint::AnyPath)]
     pub source: String,
-    /// Target image 
+
+    /// Target image or directory
+    #[arg(value_hint = ValueHint::AnyPath)]
     pub target: String,
 
-    #[clap(long, short, conflicts_with = "color")]
     /// Make the background border a blurred version of the image
+    #[arg(long, short, conflicts_with = "color")]
     pub blur: Option<f32>,
 
-    #[clap(long, short, requires = "blur", allow_hyphen_values = true)]
     /// Make the blurred background darker
+    #[arg(long, short, requires = "blur", allow_hyphen_values = true)]
     pub adjust_brightness: Option<i32>,
 
-    #[clap(long, short, conflicts_with = "blur", value_parser = parse_color)]
     /// Use this color for the background
+    #[arg(long, short, conflicts_with = "blur", value_parser = parse_color)]
     pub color: Option<Rgba<u8>>
 }
-
 
 fn parse_color(text: &str) -> anyhow::Result<Rgba<u8>> {
     let reg = Regex::new("#[0-9a-f]{6}")?;
 
     match text.to_lowercase().as_str() {
-        "black" => Ok(*Rgba::from_slice(&[0,0,0,0])),
-        "white" => Ok(*Rgba::from_slice(&[0xFF,0xFF,0xFF,0])),
+        "black" => Ok(*Rgba::from_slice(&[0, 0, 0, 0])),
+        "white" => Ok(*Rgba::from_slice(&[0xFF, 0xFF, 0xFF, 0])),
 
         text if reg.is_match(text) => parse_rgb(text),
         
--- instastack/src/args.rs
+++ instastack/src/args.rs
@@ -1,12 +1,14 @@
 use clap::Parser;
 
+/// Takes the source images and makes a new image where they are stacked vertically.
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
-/// Takes the source images and makes a new image where they are stacked vertically.
 pub struct InstastackArgs {
-    #[structopt(required = true)]
     /// The images you want to stack
+    #[structopt(required = true)]
+    #[arg(num_args = 2..)]
     pub source: Vec<String>,
+
     /// The resulting image
     pub target: String
 }
